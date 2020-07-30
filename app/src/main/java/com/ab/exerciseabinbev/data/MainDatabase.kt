45adc5bccd9015afa41c7585643f6f7e5fa221f7
Marcio Renato Covre <mcovre@motorola.com>
2020-07-30 13:44:40 -0300

    populate database only on create

--- app/src/main/java/com/ab/exerciseabinbev/data/MainDatabase.kt
+++ app/src/main/java/com/ab/exerciseabinbev/data/MainDatabase.kt
@@ -19,8 +19,8 @@ public abstract class MainDatabase : RoomDatabase() {
         private val scope: CoroutineScope
     ) : RoomDatabase.Callback() {
 
-        override fun onOpen(db: SupportSQLiteDatabase) {
-            super.onOpen(db)
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
             INSTANCE?.let { database ->
                 scope.launch {
                     val productDao = database.productDao()
